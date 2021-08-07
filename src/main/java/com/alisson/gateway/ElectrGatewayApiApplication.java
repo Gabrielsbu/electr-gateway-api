a7216d89afc5fcaca831b25d1aeffe18baf74eb3
Gabriel Ramos <gabrielmaiasbu@gmail.com>
2021-08-07 17:37:21 -0300

    Cloud gateway configurado

---
+++ src/main/java/com/alisson/gateway/ElectrGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.alisson.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrGatewayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectrGatewayApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alisson/gateway/ElectrGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisson.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrGatewayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
